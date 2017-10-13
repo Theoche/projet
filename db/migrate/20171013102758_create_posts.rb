ab79796b6dcb2b3472fad9bbbc43850e5ef60a35
theo.cherel <theo.cherel@gmail.com>
2017-10-13 10:04:44 -0400

    page students

--- db/migrate/20171013102758_create_posts.rb
+++ db/migrate/20171013102758_create_posts.rb
@@ -1,6 +1,6 @@
 class CreatePosts < ActiveRecord::Migration
   def change
-    create_table :posts do |t|
+    create_table :students do |t|
       t.string :name
       t.string :title
       t.text :content
