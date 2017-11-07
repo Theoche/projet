2b78d1e12cf907ed0425259ffb9527764ae94c42
Theoche <32217381+Theoche@users.noreply.github.com>
2017-11-07 08:57:51 +0100

    Update towns_controller.rb

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -9,10 +9,7 @@ class TownsController < ApplicationController
 
   # GET /towns/1
   # GET /towns/1.json
-  def show
-    forecast = ForecastIO.forecast(@town.latitude, @town.longitude)
-    render locals: { resource:  forecast}
-  end
+ 
 
   # GET /towns/new
   def new
